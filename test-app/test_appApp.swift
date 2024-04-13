165415bbef238bfd569d1e3e45b805923f7a05fe
Laura Webber <93733302+laura-webber@users.noreply.github.com>
2024-04-13 09:44:49 -0700

    Initial Commit

---
+++ test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Laura Webber on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
